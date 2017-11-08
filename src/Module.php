f66f7faeaabb8259a0f53d016147f5772ac44512
Payam Naderi <naderi.payam@gmail.com>
2017-11-08 17:33:42 +0330

    code cleanup

--- src/Module.php
+++ src/Module.php
@@ -5,10 +5,16 @@ namespace Module\CliFoundation
     use Poirot\Application\Interfaces\Sapi;
     use Poirot\Application\Interfaces\iApplication;
     use Poirot\Application\Interfaces\Sapi\iSapiModule;
+    use Poirot\Application\ModuleManager\Interfaces\iModuleManager;
+    use Poirot\Loader\Autoloader\LoaderAutoloadAggregate;
+    use Poirot\Loader\Autoloader\LoaderAutoloadNamespace;
+    use Poirot\Loader\Interfaces\iLoaderAutoload;
 
 
     class Module implements iSapiModule
         , Sapi\Module\Feature\iFeatureModuleInitSapi
+        , Sapi\Module\Feature\iFeatureModuleAutoload
+        , Sapi\Module\Feature\iFeatureModuleInitModuleManager
     {
         /**
          * Init Module Against Application
@@ -39,6 +45,41 @@ namespace Module\CliFoundation
             die('>_');
         }
 
+        /**
+         * Register class autoload on Autoload
+         *
+         * priority: 1000 B
+         *
+         * @param LoaderAutoloadAggregate $baseAutoloader
+         *
+         * @return iLoaderAutoload|array|\Traversable|void
+         */
+        function initAutoload(LoaderAutoloadAggregate $baseAutoloader)
+        {
+            #$nameSpaceLoader = \Poirot\Loader\Autoloader\LoaderAutoloadNamespace::class;
+            $nameSpaceLoader = 'Poirot\Loader\Autoloader\LoaderAutoloadNamespace';
+            /** @var LoaderAutoloadNamespace $nameSpaceLoader */
+            $nameSpaceLoader = $baseAutoloader->loader($nameSpaceLoader);
+            $nameSpaceLoader->addResource(__NAMESPACE__, __DIR__);
+        }
 
+        /**
+         * Initialize Module Manager
+         *
+         * priority: 1000 C
+         *
+         * @param iModuleManager $moduleManager
+         *
+         * @return void
+         */
+        function initModuleManager(iModuleManager $moduleManager)
+        {
+            // ( ! ) ORDER IS MANDATORY
+
+            if (! $moduleManager->hasLoaded('Foundation') )
+                // Module Is Required.
+                $moduleManager->loadModule('Foundation');
+
+        }
     }
 }
